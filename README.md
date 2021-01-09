d80b5e6db9d0bfb9b414fe6cc919612949ea2f03
hcoiro <hcoiro@gmail.com>
2021-01-09 00:27:40 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# pushme
+
+Send easy push notifications with a single HTTP post request, see the website for my implementation hosted on Vercel.
+
+You a free to copy this for non-commerical use. You do need to generate your own VAPID key pair and have a Mongo DB server.
+Install the now-cli and setup the secrets in now.json
+## Project setup for Vue
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/Head.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="hero is-primary is-bold">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title is-1">
+          PushMe
+        </h1>
+        <h2 class="subtitle is-3">
+          Easy push notifications with just 1 request
+        </h2>
+        <div id="nav">
+          <router-link class="has-text-light" to="/">Home</router-link> |
+          <router-link class="has-text-light" to="/about">About</router-link> |
+          <a class="has-text-light" href="https://github.com/Bartvelp/pushme" target="_blank" rel="noopener noreferrer">Github Source</a>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Head'
+}
+</script>
